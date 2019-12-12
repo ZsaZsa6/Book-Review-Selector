afa1041eb69c7a2a3ce64b7477f5f1a9092c5cb8
Ronda D Cowen <cowenhome5@gmail.com>
2019-12-12 16:24:02 +0000

    added methods-invalid_input and added to greeting calls

--- lib/book_review/session.rb
+++ lib/book_review/session.rb
@@ -2,10 +2,15 @@ def greeting
   puts "Hello!! Welcome to Book Review Selector.\nWhich New York Times Best-selling Book review you like to read?"
   puts "You can select your book by one of the following methods:\nA. By title of book \nB. By author of book (first and last name required)"
   puts "C. By browsing through genre/format lists"
-  
-# if A call title_search
-# if B call author_search
-# if C call gf_search
+    if @input = "A" || "a"
+      title_search
+    elsif @input = "B" || "b"
+      author_search
+    elsif @input = "C" || "c"
+      gf_search
+    else
+      invalid_input
+    end
 end
 
 def title_search
@@ -13,7 +18,20 @@ def title_search
 end
 
 def author_search
+
 end
 
 def gf_search
+
+end
+
+def invalid_input
+  puts "You can select your book by one of the following methods:\nA. By title of book \nB. By author of book (first and last name required)"
+  puts "C. By browsing through genre/format lists"
+  puts "Please choose A, B, or C."
+  sleep(2)
+end
+
+def get_user_input
+  @input = gets.strip
 end
