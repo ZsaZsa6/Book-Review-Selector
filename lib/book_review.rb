40b4974ad970d6554dbb4da5513b5b79194d8d24
Ronda D Cowen <cowenhome5@gmail.com>
2019-12-12 15:50:47 +0000

    adding files and requiring them

--- lib/book_review.rb
+++ lib/book_review.rb
@@ -3,9 +3,9 @@ require 'httparty'
 require 'dotenv/load'
 
 require_relative "book_review/version"
-# require_relative "london_spas/cli"
-# require_relative "london_spas/api"
-# require_relative "london_spas/spa"
+require_relative "book_review/review"
+require_relative "book_review/api"
+
 
 module BookReview
   class Error < StandardError; end
--- lib/book_review/session.rb
+++ lib/book_review/session.rb
@@ -1,3 +1,19 @@
+def greeting
 puts "Hello!! Welcome to Book Review Selector.\nWhich New York Times Best-selling Book review you like to read?"
 puts "You can select your book by one of the following methods:\nA. By title of book \nB. By author of book (first and last name required)"
 puts "C. By browsing through genre/format lists"
+
+# if A call title_search
+# if B call author_search
+# if C call gf_search
+end
+
+def title_search
+
+end
+
+def author_search
+end
+
+def gf_search
+end
