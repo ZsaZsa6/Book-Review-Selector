931c9e5ee1e6b83998d9742a1b2165b81259d170
Ronda D Cowen <cowenhome5@gmail.com>
2019-12-13 21:18:13 +0000

    added paths to list--author, title, bslists and reviews--not sure right

--- lib/book_review/api.rb
+++ lib/book_review/api.rb
@@ -1,25 +1,22 @@
 class BookReview::Api
 
   def self.review_lists
-#         {
-#             {"Authorization" => "Bearer #{ENV['YELP_API_KEY']}"},
-#         })
-#         # binding.pry - this is a great place to debug. See what `resp` is - is it the data you want?
-#         #                                               Is it a message saying you forgot some authorization?
-#         spas = resp["businesses"]
-#         LondonSpas::Spa.new_from_collection(spas)
+#   resp = HTTParty.get("https://api.nytimes.com/svc/books/v3/reviews.json?isbn=#{isbn13}&api-key=#{ENV["API_KEY"]}")
+#         reviews = resp["reviews"]
+#         BookReview::Review.new_from_collection(reviews)
 # end
-def self.title_lists
+  def self.title_lists
+#   resp = HTTParty.get("https://api.nytimes.com/svc/books/v3/reviews.json?title=#{title}&api-key=#{ENV["API_KEY"]}")
+      # title = resp["title"]
+  end
 
-#
-
-end
-
-def self.author_lists
-# resp = HTTParty.get("https://api.nytimes.com/svc/books/v3/reviews.json?author=#{author}&api-key=#{ENV['API_KEY']}")
+  def self.author_lists
+#   resp = HTTParty.get("https://api.nytimes.com/svc/books/v3/reviews.json?author=#{author}&api-key=#{ENV["API_KEY"]}")
+#    author = resp["author"]
 end
 
 def self.bs_lists
-  resp = HTTParty.get("https://api.nytimes.com/svc/books/v3/lists/names.json")
+  # resp = HTTParty.get("https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=#{ENV["API_KEY"]}")
+  #   bs_lists = resp["names"]
 
-end
+  end
