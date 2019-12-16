12141b2665e7b8946c774076bed04f6713cb777d
Ronda D Cowen <cowenhome5@gmail.com>
2019-12-16 21:15:54 +0000

    working on permission denied

--- lib/book_review/session.rb
+++ lib/book_review/session.rb
@@ -1,4 +1,5 @@
 class BookReview::Session
+  
   def greeting
     puts "Hello!! Welcome to Book Review Selector.\nWhich New York Times Best-selling Book review you like to read?"
     puts "You can select your book by one of the following methods:\nA. By title of book \nB. By author of book (first and last name required)"
