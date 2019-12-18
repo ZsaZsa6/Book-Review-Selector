b0cf717e183a31938c4d1fa10cc379adf994a91e
Ronda D Cowen <cowenhome5@gmail.com>
2019-12-18 04:10:40 +0000

    adding to author class

--- lib/book_review/api.rb
+++ lib/book_review/api.rb
@@ -1,19 +1,19 @@
 class BookReview::Api
 
   def self.review_lists
-#   resp = HTTParty.get("https://api.nytimes.com/svc/books/v3/reviews.json?isbn=#{isbn13}&api-key=#{ENV["API_KEY"]}")
-#         reviews = resp["reviews"]
-#         BookReview::Books.review(reviews)
+  resp = HTTParty.get("https://api.nytimes.com/svc/books/v3/reviews.json?isbn=#{isbn13}&api-key=#{ENV["API_KEY"]}")
+        reviews = resp["reviews"]
+        BookReview::Books.review(reviews)
 end
   def self.title_lists
   resp = HTTParty.get("https://api.nytimes.com/svc/books/v3/reviews.json?title=Becoming&api-key=#{ENV["API_KEY"]}")
       title = resp["reviews"]
   end
 
-  def self.author_lists
-#   resp = HTTParty.get("https://api.nytimes.com/svc/books/v3/reviews.json?author=#{author}&api-key=#{ENV["API_KEY"]}")
-#    author = resp["author"]
-#     BookReview::Author.book_list
+#   def self.author_lists
+# #   resp = HTTParty.get("https://api.nytimes.com/svc/books/v3/reviews.json?author=#{author}&api-key=#{ENV["API_KEY"]}")
+# #    author = resp["author"]
+# #     BookReview::Author.book_list
 
 end
 
