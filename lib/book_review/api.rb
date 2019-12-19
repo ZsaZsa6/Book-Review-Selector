506843085632c17499ac230025da968f8f996d02
Ronda D Cowen <cowenhome5@gmail.com>
2019-12-19 03:01:43 +0000

    Just some deletions in API class

--- lib/book_review/api.rb
+++ lib/book_review/api.rb
@@ -14,17 +14,4 @@ class BookReview::Api
       binding.pry
       BookReview::Book.title("titles")
   end
-
-#   def self.author_lists
-# #   resp = HTTParty.get("https://api.nytimes.com/svc/books/v3/reviews.json?author=#{author}&api-key=#{ENV["API_KEY"]}")
-# #    author = resp["author"]
-# #     BookReview::Author.book_list
-
-# end
-
-# def self.bs_lists
-#   # resp = HTTParty.get("https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=#{ENV["API_KEY"]}")
-#   #   bs_lists = resp["names"]
-#
-#   end
 end
