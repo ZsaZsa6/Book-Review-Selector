bffe3fb29fc3fedf1dccbe048f6f0136f4df29dc
Ronda D Cowen <cowenhome5@gmail.com>
2019-12-19 02:54:14 +0000

    Added to session and api to work on calls

--- lib/book_review/api.rb
+++ lib/book_review/api.rb
@@ -1,16 +1,16 @@
 class BookReview::Api
 
   def self.isbn_lists
-  resp = HTTParty.get("https://api.nytimes.com/svc/books/v3/reviews.json?isbn=#{isbn13}&api-key=#{ENV["API_KEY"]}")
+      resp = HTTParty.get("https://api.nytimes.com/svc/books/v3/reviews.json?isbn=#{isbn13}&api-key=#{ENV["API_KEY"]}")
         reviews = resp["reviews"]
         BookReview::Book.review(reviews)
         # binding.pry
     end
   def self.title_lists
-  resp = HTTParty.get("https://api.nytimes.com/svc/books/v3/reviews.json?title=Becoming&api-key=#{ENV["API_KEY"]}")
-      title = resp["reviews"]
-      book = book.new
-      resp = resp["results"][0]["book_title"]["book_author"]["summary"]["isbn13"][0]
+      resp = HTTParty.get("https://api.nytimes.com/svc/books/v3/reviews.json?title=#{"title"}&api-key=#{ENV["API_KEY"]}")
+
+      book_title = resp["results"][0]["book_title"]["book_author"]["summary"]["isbn13"][0]
+      book = BookReview::Book.new
       binding.pry
       BookReview::Book.title("titles")
   end
--- lib/book_review/session.rb
+++ lib/book_review/session.rb
@@ -1,53 +1,41 @@
 class BookReview::Session
-
   def greeting
     puts "Hello!! Welcome to Book Review Selector.\nWhich New York Times Best-selling Book review you like to read?"
     puts "You can select your book by one of the following methods:\nA. By title of book \nB. By the thirteen digit ISBN."
     puts "Please type A or B."
     puts "To exit, enter 'exit'"
-      get_user_input
+
+    @input = gets.strip
 
     if @input = "A" || "a"
         title_search
       elsif @input = "B" || "b"
-
-      # elsif @input = "B" || "b"
-        # author_search
-      # elsif @input = "C" || "c"
-        # gf_search
+        isbn_search
       else
         invalid_input
     end
+
       if @input = "exit"
         goodbye
       end
   end
-# binding.pry
+end
+
   def title_search
     puts "Please enter the full title of the book:"
-    get_user_input
-    BookReview::Api.title_lists
+    @input = gets.strip
     title = @input
+    BookReview::Api.title_lists(title)
 
+  end
+  def confirmation
     put "Is this the book you would like to review? Type yes or no."
       get_user_input
       if @input = "y" || "yes" || "Y" || "Yes"
         Book_Review::Api.
+    end
   end
 
-  def author_search
-      puts "Please enter the first and last name of the author:"
-      get_user_input
-      @input = author
-
-      puts "Select the number of the book to see the review:"
-
-  end
-
-  def gf_search
-
-
-  end
 
   def invalid_input
     puts "You can select your book by one of the following methods:\nA. By title of book \nB. By author of book (first and last name required)"
