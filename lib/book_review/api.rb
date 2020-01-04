74334c057ad605a380092082f36b30234b8db2eb
Ronda D Cowen <cowenhome5@gmail.com>
2020-01-04 17:34:00 +0000

    Fixed ISBN call

--- lib/book_review/api.rb
+++ lib/book_review/api.rb
@@ -4,7 +4,12 @@ class BookReview::Api
       resp = HTTParty.get("https://api.nytimes.com/svc/books/v3/reviews.json?isbn=#{isbn13}&api-key=#{ENV["API_KEY"]}")
         book = resp["results"][0]
         # binding.pry
-        BookReview::Book.new_from_collection(book)
+        book_title = book["book_title"]
+        book_author = book["book_author"]
+        summary = book["summary"]
+        isbn13 = book["isbn13"][0]
+        BookReview::Book.new(book_title, book_author, summary, isbn13)
+
     end
   def self.title_lists(title)
       resp = HTTParty.get("https://api.nytimes.com/svc/books/v3/reviews.json?title=#{title}&api-key=#{ENV["API_KEY"]}")
